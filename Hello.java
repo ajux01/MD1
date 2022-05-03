e5bc8e32e2351dc300ad969e4b9872114f8e1372
ajux01 <104294732+ajux01@users.noreply.github.com>
2022-05-03 23:56:22 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,26 @@
-public class Hello {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public static void main (String[] args) {
-System.out.println("Hello World!");
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(
+  name = "HelloWorld"
+  description = "It prints"
+) 
+public class Hello implements Runnable {
+  public static void main (String[] args) {
+    new CommandLine(new Hello()).execute(args);
+  }
+    
+   @Override
+  public void run() {
+    System.out.println("Hello World!");
 
 }
 
